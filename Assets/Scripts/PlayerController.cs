f9cca2803eab73a3388648f1837d0fa5a3720149
p4trick37 <140328273+p4trick37@users.noreply.github.com>
2025-10-30 23:31:46 -0400

    Changed the code for the turret movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,22 +39,46 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //Movement of the turret with the mouse
     private void TurretMovement(bool shouldMove)
+    {
+        if(shouldMove == true)
+        {
+            if(MousePosition().x < 0)
+            {
+                turret.transform.rotation = Quaternion.Euler(0, 0, GetRotationMouseTracker() + 90);
+            }
+            else
+            {
+                turret.transform.rotation = Quaternion.Euler(0, 0, GetRotationMouseTracker() - 90);
+            }
+        }
+    }
+
+
+
+    private void TurretShooter(bool shouldShoot)
+    {
+        if(shouldShoot == true)
+        {
+            
+        }
+    }
+
+    //Gathers the rotation angle of the player when using the mouse
+    private float GetRotationMouseTracker()
+    {
+        float rotationAngleRad = Mathf.Atan(MousePosition().y / MousePosition().x);
+        float rotationAngleDeg = Mathf.Rad2Deg * rotationAngleRad;
+        return rotationAngleDeg;
+    }
+
+    //Gathers the mouse position with its origen at the center of the screen
+    private Vector2 MousePosition()
     {
         Vector2 centerOfScreen = new Vector2(Screen.width / 2, Screen.height / 2);
         Vector2 mousePositionInput = Input.mousePosition;
         Vector2 mousePosition = mousePositionInput - centerOfScreen;
-        if (mousePosition.x < 0)
-        {
-            float rotationAngleRad = Mathf.Atan(mousePosition.y / mousePosition.x);
-            float rotationAngleDeg = Mathf.Rad2Deg * rotationAngleRad;
-            turret.transform.rotation = Quaternion.Euler(0, 0, rotationAngleDeg + 90);
-        }
-        else
-        {
-            float rotationAngleRad = Mathf.Atan(mousePosition.y / mousePosition.x);
-            float rotationAngleDeg = Mathf.Rad2Deg * rotationAngleRad;
-            turret.transform.rotation = Quaternion.Euler(0, 0, rotationAngleDeg - 90);
-        }
+        return mousePosition;
     }
 }
